694587b168f9dc1dc701b8ba06d1774a7e16601d
Sergii Svystun <sergiisvystun@gmail.com>
2021-08-15 13:41:36 +0300

    added methods sum() and div()

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ public class App {
         System.out.println(sum(a, b));
         System.out.println(div(a, b));
         System.out.println("something else");
+        System.out.println("One more");
     }
 
     public static int sum(int a, int b) {
